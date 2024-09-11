dcf66621640be818d75efb39f7d046458c7be53f
Christian Anderson <christiananderson@macbook-air.mynetworksettings.com>
2024-09-10 21:49:22 -0400

    sessions and rounds models

---
+++ sessions_rounds/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Sessions(models.Model):
+    month_year = models.CharField(max_length=5)
+    created_at = models.DateTimeField(auto_now_add=True)
+    closed = models.BooleanField(default=False)
+    deleted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'sessions'
+
+class RoundOptions(models.IntegerChoices):
+    ONE = 1
+    TWO = 2
+
+class Rounds(models.Model):
+    session_id = models.ForeignKey(Sessions, on_delete=models.CASCADE)
+    round_number = models.IntegerField(choices=RoundOptions.choices)
+    reated_at = models.DateTimeField(auto_now_add=True)
+    closed = models.BooleanField(default=False)
+    deleted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'rounds'
+
+
+
+
--- tome/settings.py
+++ tome/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'achievements',
+    'sessions_rounds',
     'rest_framework'
 ]
 
